27804fe2fe752f74506deefdcd326409b84827db
Satyansh Sagar <Satyansh.Sagar@carrier.com>
2023-07-19 12:25:54 +0530

    using default model, chatmodel having issues

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def main():
         docs = knowledge_base.similarity_search(user_question)
         
         #llm = OpenAI()
-        llm = AzureOpenAI(deployment_name="doc-search-gpt-35-dev",model_name="gpt-3.5-turbo")
+        llm = AzureOpenAI(deployment_name="doc-search-gpt-35-dev",model_name="gpt-35-turbo")
         chain = load_qa_chain(llm, chain_type="stuff")
         with get_openai_callback() as cb:
           response = chain.run(input_documents=docs, question=user_question)
